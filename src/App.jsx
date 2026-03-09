adf86ebd61d44ca2c410305bec0725974a09a09e
Hasset fasil <hassetfasil52@gmail.com>
2026-03-09 16:35:50 +0300

    Inital react project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello, I'm Joy 👋</h1>
+      <p>I just built my first React app!</p>
+      <p>This is the beginning of my web developer portfolio 🚀</p>
+    </div>
+  );
+}
+
+export default App;
